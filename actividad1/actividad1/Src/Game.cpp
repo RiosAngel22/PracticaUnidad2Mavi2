fce8caba4d5cd002c8f5333d23039e0f16529b68
RiosAngel22 <angelagustin1222@gmail.com>
2025-04-05 18:24:00 -0300

    agregada actividad 1

---
+++ actividad1/actividad1/Src/Game.cpp
@@ -0,0 +1,184 @@
+#include "Game.h"
+#include "Box2DHelper.h"
+
+// Constructor de la clase Game
+Game::Game(int ancho, int alto, std::string titulo)
+{
+    // Inicialización de la ventana de renderizado
+    wnd = new RenderWindow(VideoMode(ancho, alto), titulo);
+    wnd->setVisible(true);
+    fps = 60;
+    wnd->setFramerateLimit(fps);
+    frameTime = 1.0f / fps;
+    SetZoom(); // Configuración del zoom de la cámara
+    InitPhysics(); // Inicialización del mundo físico
+}
+
+// Método principal que maneja el bucle del juego
+void Game::Loop()
+{
+    while (wnd->isOpen())
+    {
+        wnd->clear(clearColor); // Limpia la ventana con un color especificado
+        DoEvents(); // Procesa los eventos del sistema
+        UpdatePhysics(); // Actualiza la simulación física
+        DrawGame(); // Dibuja el juego en la ventana
+        wnd->display(); // Muestra la ventana renderizada
+    }
+}
+
+// Actualiza la simulación física
+void Game::UpdatePhysics()
+{
+    phyWorld->Step(frameTime, 8, 8); // Avanza la simulación física
+    phyWorld->ClearForces(); // Limpia las fuerzas aplicadas a los cuerpos
+    phyWorld->DebugDraw(); // Dibuja el mundo físico (para depuración)
+}
+
+// Dibuja los elementos del juego en la ventana
+void Game::DrawGame()
+{
+    // Dibujamos el suelo
+    sf::RectangleShape groundShape(sf::Vector2f(500, 5));
+    groundShape.setFillColor(sf::Color::Red);
+    groundShape.setPosition(0, 95);
+    wnd->draw(groundShape);
+
+    // Dibujamos las paredes
+    sf::RectangleShape leftWallShape(sf::Vector2f(10, alto)); // Alto de la ventana
+    leftWallShape.setFillColor(sf::Color::Red);
+    leftWallShape.setPosition(100, 0); // X = 100 para que comience donde termina el suelo
+    wnd->draw(leftWallShape);
+
+    sf::RectangleShape rightWallShape(sf::Vector2f(10, alto)); // Alto de la ventana
+    rightWallShape.setFillColor(sf::Color::Red);
+    rightWallShape.setPosition(90, 0); // X = 90 para que comience donde termina el suelo
+    wnd->draw(rightWallShape);
+
+    sf::RectangleShape ceilingShape(sf::Vector2f(500, 5));
+    ceilingShape.setFillColor(sf::Color::Red);
+    ceilingShape.setPosition(0, 0);
+    wnd->draw(ceilingShape);
+
+
+    controlBodyAvatar->Actualizar(); // Actualiza la posición del avatar
+    controlBodyAvatar->Dibujar(*wnd); // Dibuja el avatar en la ventana
+}
+
+// Procesa los eventos del sistema
+void Game::DoEvents()
+{
+    Event evt;
+    while (wnd->pollEvent(evt))
+    {
+        switch (evt.type)
+        {
+        case Event::Closed:
+            wnd->close(); // Cierra la ventana
+            break;
+        case Event::MouseButtonPressed:
+            // Crea un cuerpo dinámico en forma de triángulo en la posición del clic del ratón
+            b2Body* body = Box2DHelper::CreateTriangularDynamicBody(phyWorld, b2Vec2(0.0f, 0.0f), 10.0f, 1.0f, 4.0f, 0.1f);
+            // Transforma las coordenadas según la vista activa
+            Vector2f pos = wnd->mapPixelToCoords(Vector2i(evt.mouseButton.x, evt.mouseButton.y));
+            body->SetTransform(b2Vec2(pos.x, pos.y), 0.0f);
+            break;
+        }
+    }
+
+    // Mueve el cuerpo controlado por el teclado
+    controlBody->SetAwake(true); // Activa el cuerpo para que responda a fuerzas y colisiones
+    if (Keyboard::isKeyPressed(Keyboard::Left))
+        controlBody->SetLinearVelocity(b2Vec2(-50.0f, 0.0f));
+    if (Keyboard::isKeyPressed(Keyboard::Right))
+        controlBody->SetLinearVelocity(b2Vec2(50.0f, 0.0f));
+    if (Keyboard::isKeyPressed(Keyboard::Down))
+        controlBody->SetLinearVelocity(b2Vec2(0.0f, 50.0f));
+    if (Keyboard::isKeyPressed(Keyboard::Up))
+        controlBody->SetLinearVelocity(b2Vec2(0.0f, -50.0f));
+}
+
+// Configura el área visible en la ventana de renderizado
+void Game::SetZoom()
+{
+    View camara;
+    camara.setSize(100.0f, 100.0f); // Tamaño del área visible
+    camara.setCenter(50.0f, 50.0f); // Centra la vista en estas coordenadas
+    wnd->setView(camara); // Asigna la vista a la ventana
+}
+
+// Inicializa el mundo físico y los elementos estáticos del juego
+void Game::InitPhysics()
+{
+    // Inicializa el mundo físico con la gravedad por defecto
+    phyWorld = new b2World(b2Vec2(0.0f, 9.8f));
+
+    // Inicializa el renderizador de depuración para el mundo físico
+    debugRender = new SFMLRenderer(wnd);
+    debugRender->SetFlags(UINT_MAX); // Configura el renderizado para que muestre todo
+    phyWorld->SetDebugDraw(debugRender);
+
+    // Crea los elementos estáticos del juego (suelo y paredes)
+    b2Body* groundBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 100, 10);
+    groundBody->SetTransform(b2Vec2(50.0f, 100.0f), 0.0f);
+
+    //Creamos superficie elastica
+    b2FixtureDef suelo;
+    suelo.restitution = 1;
+
+    b2PolygonShape sueloShape;
+    sueloShape.SetAsBox(100, 5);
+    suelo.shape = &sueloShape;
+
+    b2Fixture sueloFixture = *groundBody->CreateFixture(&suelo);
+
+    b2Body* ceilingBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 100, 10);
+    ceilingBody->SetTransform(b2Vec2(50.0f, 0.0f), 0.0f);
+
+    //Creamos superficie elastica
+    b2FixtureDef techo;
+    techo.restitution = 1;
+
+    b2PolygonShape techoShape;
+    techoShape.SetAsBox(100, 5);
+    techo.shape = &techoShape;
+
+    b2Fixture techoFixture = *ceilingBody->CreateFixture(&techo);
+  
+    b2Body* leftWallBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 10, 100);
+    leftWallBody->SetTransform(b2Vec2(0.0f, 50.0f), 0.0f);
+
+    //Creamos pared elastica
+    b2FixtureDef pared;
+    pared.restitution = 1;
+
+    b2PolygonShape paredShape;
+    paredShape.SetAsBox(5, 100);
+    pared.shape = &paredShape;
+
+    b2Fixture paredFixture = *leftWallBody->CreateFixture(&pared);
+
+    b2Body* rightWallBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 10, 100);
+    rightWallBody->SetTransform(b2Vec2(100.0f, 50.0f), 0.0f);
+
+    //Creamos pared elastica
+    b2FixtureDef pared2;
+    pared2.restitution = 1;
+
+    b2PolygonShape pared2Shape;
+    pared2Shape.SetAsBox(5, 100);
+    pared2.shape = &pared2Shape;
+
+    b2Fixture pared2Fixture = *rightWallBody->CreateFixture(&pared2);
+
+    // Crea un cuerpo de círculo controlado por el teclado
+    controlBody = Box2DHelper::CreateCircularDynamicBody(phyWorld, 5, 1.0f, 0.5, 0.5f);
+    controlBody->SetTransform(b2Vec2(50.0f, 50.0f), 0.0f);
+
+    // Carga la textura de la pelota para el avatar
+    texturaPelota.loadFromFile("Pelota.png");
+
+    // Inicializa el avatar del jugador con el cuerpo físico creado y la textura de la pelota
+    controlBodyAvatar = new Avatar(controlBody, new sf::Sprite(texturaPelota));
+}
+
